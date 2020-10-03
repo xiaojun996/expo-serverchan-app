7fca87ff2664dfbf04a8f96cb5f486f41c28452d
xiaojun996 <blueskyzx@icloud.com>
2020-10-03 18:08:12 +0800

    fix

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,21 +2,21 @@
  * 颜色常量
  */
 const tintColorLight = '#88def9'
-const tintColorDark = '#fff'
+const tintColorDark = '#88def9'
 
 export default {
   light: {
     text: '#000',
     background: '#ededed',
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: '#eee',
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: '#fff',
     background: '#121212',
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: '#eee',
     tabIconSelected: tintColorDark,
   },
 }
