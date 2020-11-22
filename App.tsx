b9344328f62332ab6431e9aeabb4ec7433249c5d
xiaojun996 <blueskyzx@icloud.com>
2020-11-22 15:21:31 +0800

    chore

--- App.tsx
+++ App.tsx
@@ -10,14 +10,10 @@ export default function App() {
   const isLoadingComplete = useCachedResources()
   const colorScheme = useColorScheme()
 
-  if (!isLoadingComplete) {
-    return null
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    )
-  }
+  return !isLoadingComplete ? null : (
+    <SafeAreaProvider>
+      <Navigation colorScheme={colorScheme} />
+      <StatusBar />
+    </SafeAreaProvider>
+  )
 }
